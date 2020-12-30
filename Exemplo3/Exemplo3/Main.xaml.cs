320d04eab6874d9a22288db298bb36e6aab9fae7
Pedro Carvalho <pedrosilvacar@gmail.com>
2020-12-30 19:06:32 +0000

    -

--- Exemplo3/Exemplo3/Main.xaml.cs
+++ Exemplo3/Exemplo3/Main.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -12,7 +13,11 @@ namespace Exemplo3
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Main : ContentPage
     {
+        readonly CancellationTokenSource s_cts = new CancellationTokenSource();
         bool runtask = false;
+        int primeiro = 10000;
+        int segundo  = 99999;
+
         public Main()
         {
             InitializeComponent();
@@ -29,7 +34,15 @@ namespace Exemplo3
                 //a tarefa esta a ser executada
                 Barra.IconImageSource = ImageSource.FromResource("Exemplo3.Resources.stop.ico");
                 runtask = true;
+                //Task tarefa = 
             }
+        }
+
+        async Task Tarefa()
+        {
+            if (!runtask) s_cts.Cancel();
+            primeiro++;
+            segundo--;
         }
     }
 }
